cabea2983f4f86253eb8b8777ed12875c3dcdb54
Swapnoneel Majumdar <swapnoneelmajumdar@gmail.com>
2022-04-23 21:07:36 +0530

    Initial commit

---
+++ src/Question.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+const Question = ({title,info}) => {
+  
+  const [isOpen,setIsOpen] = useState(false);
+
+  return <article className='question'>
+     <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={()=>setIsOpen(!isOpen)}>
+           {!isOpen ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+     </header>
+     {isOpen && info}
+  </article>;
+};
+
+export default Question;
